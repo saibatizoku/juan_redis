ab3202171321933b25b315a11a644cff3854c687
Joaquin R <globojorro@gmail.com>
2017-02-18 01:10:55 -0600

    replicate redis client connection example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate redis;
+use std::error::Error;
+use redis::Commands;
+
+
+fn do_set_key(con: &redis::Connection) -> redis::RedisResult<()> {
+    let key = "the_key";
+    let _: () = try!(con.set(key, 42));
+    println!("Key set!: {}, {}", key, 42);
+
+    Ok(())
+}
+
+fn do_run_code() -> redis::RedisResult<()> {
+    let client = try!(redis::Client::open("redis://redis.juan.libres:6379/"));
+    let con = try!(client.get_connection());
+
+    try!(do_set_key(&con));
+
+    Ok(())
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    match do_run_code() {
+      Err(err) => {
+        println!("Could not execute libres redis client:");
+        println!("  {}: {}", err.category(), err.description());
+      }
+      Ok(()) => {}
+    }
 }
